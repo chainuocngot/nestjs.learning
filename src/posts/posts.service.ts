50e29c9108ce90281090ef0219dedecd9a267747
dvanh.hxg <anhvu.dev.103@gmail.com>
2026-05-30 13:40:15 +0700

    Bài 9: Làm quen GET POST PUT DELETE

---
+++ src/posts/posts.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PostsService {
+  getPosts() {
+    return 'All posts';
+  }
+
+  createPost(body: any) {
+    return body;
+  }
+
+  getPost(id: string) {
+    return `Post with id ${id}`;
+  }
+
+  updatePost(id: string, body: any) {
+    return `Updated post with id ${id} and body ${JSON.stringify(body)}`;
+  }
+
+  deletePost(id: string) {
+    return `Deleted post with id ${id}`;
+  }
+}
